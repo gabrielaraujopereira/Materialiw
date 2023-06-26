ae719ba9ac2465df24f6ca12ed30580d73c26168
gabrielaraujopereira <detonator834@gmail.com>
2023-06-26 18:28:29 -0300

    a

---
+++ script.js
@@ -0,0 +1,15 @@
+function pesquisar(){
+    var input,filtro,personagens,persoItens,links;
+    input = document.getElementById("pesquisa");
+    filtro = input.value.toUpperCase();
+    personagens = document.getElementById("personagens");
+    persoItens = personagens.getElementsByTagName("li");
+    for(var i=0;i<persoItens.length;i++){
+        links = persoItens[i].getElementsByTagName("a")[0];
+        if(links.innerHTML.toUpperCase().indexOf(filtro)>-1){
+            persoItens[i].style.display="";
+        }else{
+            persoItens[i].style.display="none";
+        }
+    }
+}
