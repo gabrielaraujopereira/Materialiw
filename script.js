d094d667dbf54fb39de5daf437040482d9dccee5
gabrielaraujopereira <detonator834@gmail.com>
2023-06-27 19:20:54 -0300

    a

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function pesquisar(){
         }
     }
 }
+
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('nav');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('open');
+}
