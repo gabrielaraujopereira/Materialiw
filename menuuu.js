ee5cc12ccf729317ddd3b670350403d7f7f4cab1
gabrielaraujopereira <detonator834@gmail.com>
2023-06-29 14:44:53 -0300

    a

--- menuuu.js
+++ menuuu.js
@@ -3,7 +3,7 @@ var menuBtn = document.querySelector('.menu-btn i');
 
 function menuShow(){
     if (ul.classList.contains('open')) {
-        (ul.classList.remove('open'));
+        ul.classList.remove('open');
     }
     else{
         ul.classList.add('open');
