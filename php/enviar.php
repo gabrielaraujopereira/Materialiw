c4c25068b49af16aca5b25ef32b6151bb78208bd
gabrielaraujopereira <detonator834@gmail.com>
2023-07-03 14:27:03 -0300

    a

--- php/enviar.php
+++ php/enviar.php
@@ -1,21 +1,26 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $mensagem = $_POST['mensagem'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
 
-    $para = 'detonator834@gmail.com';
-    $assunto = 'Formulário de Contato';
-    $corpo = "Nome: $nome\nEmail: $email\nMensagem: $mensagem";
-    $headers = "From: $email";
+$para = 'detonator834@gmail.com'; 
+$assunto = 'Nova mensagem de contato';
 
-    if (mail($para, $assunto, $corpo, $headers)) {
-        echo 'Obrigado por entrar em contato! Sua mensagem foi enviada.';
-    } else {
-        echo 'Desculpe, ocorreu um erro ao enviar sua mensagem.';
-    } else {
-    header("Location: contato.html");
-    exit();
+$corpo = "Nome: $nome\n";
+$corpo .= "Email: $email\n";
+$corpo .= "Mensagem: $mensagem\n";
 
+if (empty($nome) || empty($email) || empty($mensagem)) {
+    echo "Por favor, preencha todos os campos.";
+    exit;
+}
+
+if (mail($para, $assunto, $corpo)) {
+
+  header("Location: contato.html"); 
+  exit;
+} else {
+ 
+  echo "Ocorreu um erro ao enviar o email.";
 }
 ?>
